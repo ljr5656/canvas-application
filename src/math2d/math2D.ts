340e0a86fb8b750fae70dfd09cbe434b7e0ab2be
ljr <1368298676@qq.com>
2023-06-05 11:51:10 +0800

    math2d 弧度角度转换

--- src/math2d/math2D.ts
+++ src/math2d/math2D.ts
@@ -1,5 +1,6 @@
 export default class math2D {
   public static EPSILON: number = 0.00001;
+  public static PiBy180: number = 0.017453292519943295;
   public static isEquals(
     left: number,
     right: number,
@@ -9,5 +10,9 @@ export default class math2D {
       return false;
     }
     return true;
+  }
+
+  public static toDegree(radian: number): number {
+    return radian / math2D.PiBy180;
   }
 }
