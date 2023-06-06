f042eb5369b99040c4240d03d43c1a1256412a15
ljr <1368298676@qq.com>
2023-06-06 09:52:58 +0800

    canvas 绘制矩形

--- src/base/canvasApplication.ts
+++ src/base/canvasApplication.ts
@@ -9,4 +9,26 @@ export default class Canvas2DApplication extends Application {
     super(canvas);
     this.context2D = canvas.getContext('2d', contextAttribute);
   }
+
+  //矩形
+  protected _drawRect(x: number, y: number, w: number, h: number): void {
+    const { context2D } = this;
+    if (context2D === null) {
+      return;
+    }
+    context2D.save();
+    context2D.fillStyle = 'grey';
+    context2D.strokeStyle = 'blue';
+    context2D.beginPath();
+
+    context2D.moveTo(x, y);
+    context2D.lineTo(x + w, y);
+    context2D.lineTo(x + w, y + h);
+    context2D.lineTo(x, y + h);
+
+    context2D.closePath();
+    context2D.fill();
+    context2D.stroke();
+    context2D.restore();
+  }
 }
