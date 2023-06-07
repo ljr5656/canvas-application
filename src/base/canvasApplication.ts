5ad4ad9bc55d221feafd07631df3a2b15f0fd319
ljr <1368298676@qq.com>
2023-06-07 10:47:36 +0800

    canvas _strokeLine, _strokeCoord, _strokeGrid 方法

--- src/base/canvasApplication.ts
+++ src/base/canvasApplication.ts
@@ -49,4 +49,50 @@ export default class Canvas2DApplication extends Application {
     context2D.fill();
     context2D.restore();
   }
+
+  protected _strokeLine(x0: number, y0: number, x1: number, y1: number): void {
+    const { context2D } = this;
+    if (context2D === null) return;
+    context2D.beginPath();
+    context2D.moveTo(x0, y0);
+    context2D.lineTo(x1, y1);
+    context2D.stroke();
+  }
+
+  protected _strokeCoord(
+    orginX: number,
+    orginY: number,
+    width: number,
+    height: number,
+  ): void {
+    const { context2D } = this;
+    if (context2D === null) return;
+    context2D.save();
+    context2D.strokeStyle = 'red';
+    this._strokeLine(orginX, orginY, orginX + width, orginY);
+    context2D.strokeStyle = 'blue';
+    this._strokeLine(orginX, orginY, orginX, orginY + height);
+    context2D.restore();
+  }
+
+  // grid绘制的区域为整个canvas的大小
+  // 其中参数interval控制每个网格横向和纵向的间隔大小
+  public _strokeGrid(color: string = 'grey', interval: number = 10): void {
+    const { context2D, canvas } = this;
+    if (context2D === null) return;
+    context2D.save();
+    context2D.strokeStyle = color;
+    context2D.lineWidth = 0.5;
+
+    for (let i: number = interval + 0.5; i < canvas.width; i += interval) {
+      this._strokeLine(i, 0, i, canvas.height);
+    }
+
+    for (let i: number = interval + 0.5; i < canvas.height; i += interval) {
+      this._strokeLine(0, i, canvas.width, i);
+    }
+    context2D.restore();
+    this._fillCircle(0, 0, 5, 'green');
+    this._strokeCoord(0, 0, canvas.width, canvas.height);
+  }
 }
