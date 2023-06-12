8f1e23d54a2c2dccf9e7a49b14c53ac4703b1c54
ljr <1368298676@qq.com>
2023-06-12 09:20:04 +0800

    TreeNode 添加节点, 查询节点

--- src/base/treeNode.ts
+++ src/base/treeNode.ts
@@ -4,6 +4,21 @@ export class TreeNode<T> {
   public name: string;
   public data: T | undefined;
 
+  constructor(
+    data: T | undefined = undefined,
+    parent: TreeNode<T> | undefined = undefined,
+    name: string = '',
+  ) {
+    this._parent = parent;
+    this._children = undefined;
+    this.name = name;
+    this.data = data;
+
+    if (this._parent !== undefined) {
+      this._parent.addChild(this);
+    }
+  }
+
   // this节点是否为ancestor节点的子孙节点
   public isDescendantOf(ancestor: TreeNode<T> | undefined): boolean {
     if (ancestor === undefined) {
@@ -54,9 +69,67 @@ export class TreeNode<T> {
     return this.removeChildAt(index);
   }
 
-  public getChildAt(index: number): TreeNode<T> | undefined {
-    if (this._children === undefined || this._children.length === 0) {
+  //添加节点
+  public addChildAt(
+    child: TreeNode<T> | undefined,
+    index: number,
+  ): TreeNode<T> | undefined {
+    // 如果添加的节点是当前节点的祖先节点, 会造成循环引用
+    if (this.isDescendantOf(child) || child === undefined) {
       return undefined;
     }
+    if (this._children === undefined) {
+      this._children = [];
+    }
+    if (index >= 0 && index < this._children.length) {
+      // 如果要添加的节点有父节点, 从其父节点中remove掉
+      if (child._parent !== undefined) {
+        child._parent.removeChild(child);
+      }
+
+      child._parent = this;
+      this._children.splice(index, 0, child);
+      return child;
+    } else {
+      return undefined;
+    }
+  }
+
+  public addChild(child: TreeNode<T> | undefined): TreeNode<T> | undefined {
+    if (this._children === undefined) {
+      this._children = [];
+    }
+    this.addChildAt(child, this._children.length);
+    return child;
+  }
+
+  //获取儿子节点
+  public getChildAt(index: number): TreeNode<T> | undefined {
+    if (this._children === undefined) {
+      return undefined;
+    }
+
+    return this._children[index];
+  }
+
+  //获取当前节点儿子节点数量
+  public get childCount(): number {
+    if (this._children !== undefined) {
+      return this._children.length;
+    } else {
+      return 0;
+    }
+  }
+
+  // 当前节点是否存在儿子节点
+  public hasChild(): boolean {
+    return this._children !== undefined && this._children.length > 0;
+  }
+
+  public get parent(): TreeNode<T> | undefined {
+    return this._parent;
+  }
+  public get children(): TreeNode<T>[] | undefined {
+    return this._children;
   }
 }
